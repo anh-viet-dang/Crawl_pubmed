3142af865012385cc9c9a7a9ef4def9467dc1eb4
anhdv12 <imnova1212@gmail.com>
2022-01-17 23:14:02 +0700

    add pmid

---
+++ data/README.md
@@ -0,0 +1,19 @@
+## pmid_gene.txt chứa pmid của các paper có lien quan đến gene
+Tạm thời để xác định các pmid này, lấy thủ công các file html từ hgmd, từ đó lấy ra pmid
+## pmid_not_gene.txt chứa pmid của các paper ko liên quan đến gen
+crawl random từ pubmed
+## folder paper chứa các file có format
+
+### tên file : pmid + .txt
+
+### nội dung:
+
+- line 1 : pmid
+- line 2 : title
+- line 3 : abstract
+- line 3 có thể ko có nội dung , do 1 số lượng paper ko có abstract
+
+
+### Các nguồn để crawl paper
+- HGMD -> pmid -> pubmed
+- pubmed -> pmid
--- lib/utils.py
+++ lib/utils.py
@@ -6,26 +6,6 @@ from bs4.element import Tag
 
 from colorama import Fore
 
-
-
-# desktop_agents = [
-#     'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36',
-#     'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36',
-#     'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36',
-#     'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/602.2.14 (KHTML, like Gecko) Version/10.0.1 Safari/602.2.14',
-#     'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36',
-#     'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.98 Safari/537.36',
-#     'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.98 Safari/537.36',
-#     'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36',
-#     'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36',
-#     'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:50.0) Gecko/20100101 Firefox/50.0'
-# ]
-
-# def random_headers(User_Agent:list= desktop_agents)->dict:
-#     return {'User-Agent': choice(User_Agent),
-#             'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8'}
-
-
 def send_request(url: str)->Tag:
     r"""
          ____________________________________________________
