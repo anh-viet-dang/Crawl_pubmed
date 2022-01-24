d0f7b1607fd40a640b9235611d91d26805c33e52
anhdv12 <imnova1212@gmail.com>
2022-01-24 19:25:20 +0700

    fix bug

--- list_faaa.py
+++
@@ -1,9 +0,0 @@
-folder = "/home/agent/Documents/AVADA/Pubmed-Batch-Download/fetched_pdfs"
-
-import os
-from glob import glob
-
-x = glob(folder + '/*.pdf')
-print(len(x))
-y = glob(folder + '/*.html')
-print(len(y))
