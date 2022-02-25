9808bdae4b5c77485f0b88db2073837e8d294c3b
anhdv12 <imnova1212@gmail.com>
2022-02-25 17:55:26 +0700

    download full pdf from scihub

--- usingDOI_download_Scihub.py
+++ usingDOI_download_Scihub.py
@@ -1,45 +1,57 @@
+"""
+dùng doi để download fulltext từ scihub
+"""
 from os.path import isfile, join
+import os
 from pdfminer.high_level import extract_text
 from lib.scihub import Scihub
 from colorama import Fore
+from lib.config import PMID2DOI_FILE_PATH, PMID_HGMD, PMID_SIMI, PMID_NEGA
 
+folder_save_pdf = "data/fulltext/fetched_pdfs"
 
-def download_with_unfetch_pdf():
-    pmid_path = "/home/agent/Documents/AVADA/Crawl_pubmed/data/unfetched_pmid.txt"
-    pdf_path = "/home/agent/Documents/AVADA/Crawl_pubmed/data/fetched_pdfs"
-
-
-    with open(pmid_path, 'r', encoding= 'utf-8') as f:
+def get_list_pmid(path:str) -> list:
+    """
+    load file crawl title, abstract để lấy pmid
+    """
+    with open(path, 'r', encoding= 'utf-8') as f:
         lines = f.read().strip().split('\n')
-
-    for pmid in lines:
-        path_save = join(pdf_path, pmid + ".pdf")
-        if not isfile(path_save):
-            doi = Scihub.pmid2doi(pmid)
-            resp_code = Scihub.download_scihub(doi, path_save)
-            print(Fore.RED + pmid, ' -> ',Fore.MAGENTA + str(resp_code), ' -> ',Fore.GREEN + doi)
-
-
-def download_with_cannot_convertpdf():
-    pmid_path = r"/home/agent/Documents/AVADA/Crawl_pubmed/data/cannot_convertpdf.txt"
-    pdf_path = r"/home/agent/Documents/AVADA/Crawl_pubmed/data/fetched_pdfs"
     
+    pmids = []
+    for i, line in enumerate(lines):
+        if i%4 == 0:
+            if not line.isdecimal():
+                print(Fore.CYAN + str(i), Fore.BLUE + line)  #PMID
+                print(Fore.RED + lines[i+1])
+                print(Fore.LIGHTGREEN_EX + lines[i+2])
+                exit()
+            pmids.append(line.strip())
 
-    with open(pmid_path, 'r', encoding= 'utf-8') as f:
-        lines = f.read().strip().split('\n')
-    pmids = [p.strip('.txt') for p in lines]
+    return pmids
 
-    for pmid in pmids:
-        path_pdf = join(pdf_path, pmid + ".pdf")
-        try:
-            extract_text(path_pdf)
-            print(pmid)
-            continue
-        except:
-            doi = Scihub.pmid2doi(pmid)
-            resp_code = Scihub.download_scihub(doi, path_pdf)
-            print(Fore.RED + pmid, ' -> ',Fore.MAGENTA + str(resp_code), ' -> ',Fore.GREEN + doi)
+
+def download_fulltext(pmid:str):
+    doi = Scihub.pmid2doi(pmid, is_save= True)
+    if doi != "":
+        pdf_path_save = join(folder_save_pdf, pmid + '.pdf')
+        sttcode = Scihub.download_scihub(doi= doi, path_save=pdf_path_save)
+        return sttcode, doi
+    else:
+        return 0, doi
+
+def get_crawled_fulltext_pmid():
+    return [pdf.strip('.pdf') for pdf in os.listdir(folder_save_pdf) if pdf.endswith('.pdf')]
 
 
 if __name__ == "__main__":
-    download_with_cannot_convertpdf()
+    pmid_hgmd = get_list_pmid(PMID_HGMD)
+    pmid_similar = get_list_pmid(PMID_SIMI)
+    pmid_negative = get_list_pmid(PMID_NEGA)
+
+    pmid_crawls = get_crawled_fulltext_pmid()
+    for pmid in pmid_hgmd:
+        if pmid in pmid_crawls: continue
+
+        sttcode, doi = download_fulltext(pmid)
+        if sttcode != 200:
+            print(Fore.RED + pmid + ' ' + Fore.LIGHTMAGENTA_EX + str(sttcode) + ' ' +Fore.LIGHTGREEN_EX + doi )
