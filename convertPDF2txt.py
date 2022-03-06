06eaecf1b77ae58fcf7c764567f53ca4a452666c
watson21 <imnova1212@gmail.com>
2022-03-06 17:31:42 +0700

    fix recursive limit

--- convertPDF2txt.py
+++ convertPDF2txt.py
@@ -1,7 +1,7 @@
 from os.path import isfile, isdir, join, basename
 import os
 import sys
-sys.setrecursionlimit(32000) # 1000
+sys.setrecursionlimit(48000) # 1000
 from glob import glob
 
 from pdfminer.high_level import extract_text
@@ -14,27 +14,26 @@ def cvtPdf2Txt(folderPDF, folderTXT):
     pdfs = glob(folderPDF + '/*.pdf')
     for pdf in pdfs:
         name = basename(pdf)
-        pmid_index = name.strip('.pdf')
         name = name.replace('.pdf', '.txt')
         path_txt = join(folderTXT, name)
 
         # đã convert rồi thì bỏ qua
         if isfile(path_txt):
             continue
-
+        
         try:
             text = extract_text(pdf)
             with open (path_txt, 'w', encoding='utf-8') as f:
                 f.write(text)
-            print(pmid_index)
+            print(name)
 
         except Exception as e:
             with open('data/cannot_convertpdf.txt', 'a', encoding= 'utf-8') as c:
-                c.write(pmid_index + '\n')
-            print(pmid_index, e)
+                c.write(name.strip('.txt') + '\n')
+            print(name, e)
 
 if __name__ == "__main__":
-    folderPDF = r'data/fulltext/similar_pdfs/similar_1'
-    folderTXT = r'data/fulltext/similar_txts/similar_1'
-    
+    i = 1
+    folderPDF = r'data/fulltext/similar_pdfs/similar_{}'.format(i)
+    folderTXT = r'data/fulltext/similar_txts/similar_{}'.format(i)
     cvtPdf2Txt(folderPDF, folderTXT)
